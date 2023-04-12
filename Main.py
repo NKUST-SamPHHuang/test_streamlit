1a6f4fc3445f356f42b623919b99ed3031f6fab0
DESKTOP-HJ03U5R\samch <samhuang@stust.edu.tw>
2023-04-12 15:29:25 +0800

    小改版

--- Main.py
+++ Main.py
@@ -8,9 +8,12 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import yfinance as yf
+import prophet
 from datetime import date
 from plotly import graph_objects as go
 from plotly.subplots import make_subplots
+from prophet.plot import plot_plotly, plot_components_plotly
+import matplotlib.pyplot as plt
 
 
 # @st.cache
@@ -27,6 +30,7 @@ def load_data(ticker):
         "Adj Close": "收盤價(修正後)",
         "Volume": "成交股數"
     }, axis=1, inplace=True)
+
     return data
 
 def plot_raw_data(stock_data):
@@ -57,7 +61,7 @@ def plot_raw_data(stock_data):
     fig.update_xaxes(title_text="日期")
     fig.update_yaxes(title_text="股價", row=1, col=1)
     fig.update_yaxes(title_text="成交股數", row=2, col=1)
-    st.plotly_chart(fig)
+    return fig
 
 def rename_describe(stock_data):
     return stock_data.rename({
@@ -77,52 +81,99 @@ def reform_number_format(stock_data):
         stock_data[x] = stock_data[x].map(lambda x: f"{x:,.0f}")
     return stock_data
 
+def forecasting(stock_data):
+    df = stock_data[["日期", "收盤價(修正後)"]].copy()
+    df.columns = ['ds', 'y']  # Prophet model 需特定的欄位名稱 
+    # 訓練集
+    train = df.drop(stock_data.index[-250:])
+    # 測試集
+    future = df.iloc[stock_data.index[-250:]][["ds"]]
+    future.reset_index(drop=True, inplace=True)
+    # 模型建立
+    model = prophet.Prophet()
+    model.fit(train)
+    # 模型預測
+    forecast = model.predict(future)
+    
+    return model, forecast, df
+
+def validation(df, forecast):
+    # plot results
+    fig = model.plot(forecast)
+    plt.scatter(x=df.ds, y=df.y)
+    plt.legend(['Actual', 'Predict'])
+
+    return fig
 
 # 參數設定
 TODAY_val = date.today()
 TODAY_str = TODAY_val.strftime("%Y-%m-%d")
-START_str = "2019-1-1"
+START_str = "2021-1-1"
 
-with st.spinner('股市資訊'):
-    # 版面設定
-    st.title("股市資訊")
+# Page Config
+st.set_page_config(page_title='金融大數據',  layout='wide')
+
+# 版面設定
+st.title("金融大數據")
 
 
-    # 資料取得
-
-    labeled_stock = st.text_input("輸入股票代號", value="0050")
-    data_load_state = st.text(f"讀取檔案: {labeled_stock}")
+# 資料取得
+st.subheader("讀取股價資料")
+c1, c2, c3 = st.columns((1,1,1))
+labeled_stock = c1.text_input("輸入股票代號", value="0050")
+START_str = c2.date_input("設定開始日", date(2021, 1, 1))
+TODAY_str = c3.date_input("設定結束日", TODAY_val)
+with st.spinner(f"{labeled_stock} 資料讀取中..."):
     try:
         data = load_data(f"{labeled_stock}.tw")
-        status = True
     except:
-        data_load_state = st.text("查詢失敗")
-        status = False
+        st.text("查詢失敗")
+    
+if not data.empty:
+    data_load_state = st.text(f"{labeled_stock} 資料讀取完成!")
+
+    st.subheader("資料分析")
+    with st.expander("資料摘要"):
+        temp_data = data[["日期", "開盤價", "最高價", "最低價", "收盤價"]].copy()
         
+        # 資料呈現
+        st.subheader("資料摘要")
+        st.write(reform_number_format(rename_describe(temp_data.describe())))
+        
+        m1, m2 = st.columns((1,1))
+        m1.subheader("最前5筆資料")
+        m1.write(temp_data.head())
+        m2.subheader("最後5筆資料")
+        m2.write(temp_data.tail())
+                
+    with st.expander("資料視覺化"):
+        # 資料繪圖
+        st.title("股價K線")
+        fig = plot_raw_data(data)
+        st.plotly_chart(fig, use_container_width=True)
 
-    if status:
-        if data.empty:
-            data_load_state = st.text("查無此代碼")
+    with st.expander("預測模型"):
+        # 預測模型
+        if data.shape[0] < 250:
+            st.write("資料不足，無法預測")
         else:
-            data_load_state = st.text("讀取完成...")
+            model, forecast, df = forecasting(data)
+            fig = plot_plotly(model, forecast)
+            st.plotly_chart(fig, use_container_width=True)
 
-        if not data.empty:
-            # 資料呈現
-            st.title("股價資訊")
-            st.subheader("最前5筆資料")
-            st.write(data.head())
-            st.subheader("最後5筆資料")
-            st.write(data.tail())
-            st.subheader("資料摘要")
-            st.write(reform_number_format(rename_describe(data.describe())))
-            
+    with st.expander("模型驗證"):
+        # 模型驗證
+        if data.shape[0] < 250:
+            st.write("資料不足，無法驗證")
+        else:
+            fig = validation(df, forecast)
+            st.pyplot(fig, width = 120)
 
-            # 資料繪圖
-            st.title("股價K線")
-            plot_raw_data(data)
+    with st.expander("完整資料"):
+        # 完整資料呈現
+        st.subheader(f"完整資料: {START_str} 至 {TODAY_str}")
+        st.write(data)
 
-            # 完整資料呈現
-            st.subheader(f"完整資料: {START_str} 至 {TODAY_str}")
-            st.write(data)
-
+else:
+    st.text("查無此代碼")
 
